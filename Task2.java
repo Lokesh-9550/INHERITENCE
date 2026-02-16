9739a6462ecbb1ab37b0c786cea99ff8a179cb76
Lokesh <puttamlokesh9550@gmail.com>
2026-02-16 11:42:59 +0530

    Task2

---
+++ Task2.java
@@ -0,0 +1,19 @@
+class Demo1{
+    Demo1(){
+        System.out.println("zero param constructor.");
+    }
+}
+class Demo2 extends Demo1{
+
+}
+
+public class Task2 {
+    public static void main(String[] args) {
+        Demo2 d=new Demo2();
+        
+        
+    }
+    
+}
+
+
